6f2413fb1dd44138f4dc9924e5fb8f371b7c9ed6
yefengming1995 <474005611@qq.com>
2018-06-24 19:18:44 +0800

    commit_v4

---
+++ src/app/askforleave/askforleave.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-askforleave',
+  templateUrl: './askforleave.component.html',
+  styleUrls: ['./askforleave.component.scss']
+})
+export class AskforleaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
